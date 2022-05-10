09370d59d4a19c0934999444e68984c71f3dfe7d
Marek Grabarz <m.grabarz@wp.pl>
2022-05-11 00:01:30 +0200

    Update network.tf

--- tf/network.tf
+++ tf/network.tf
@@ -11,8 +11,8 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
-resource
-"azurerm_resource_group" "rg" {
+
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = "West Europe"
 }
