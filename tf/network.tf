12e292bb0f28702b593207342b238519d16b589e
Marek Grabarz <m.grabarz@wp.pl>
2022-05-10 23:58:07 +0200

    Update network.tf

--- tf/network.tf
+++ tf/network.tf
@@ -18,7 +18,7 @@ resource
 }
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "terraform" {
-  name = "terraform-network"
+  name = var.virtual_network_name
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   address_space = ["10.10.0.0/24"]
