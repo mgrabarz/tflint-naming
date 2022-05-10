6818f38c13c0d040a2faaaf5a8fb11dca740a00b
Marek Grabarz <m.grabarz@wp.pl>
2022-05-11 00:02:38 +0200

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,5 +1,10 @@
 
-variable
-"resource_group_name" {
+variable "resource_group_name" {
   type = string
+  default = "terraform-rg01"
+}
+
+variable "virtual_network_name" {
+  type = string
+  default = "terraform-vnet01"
 }
